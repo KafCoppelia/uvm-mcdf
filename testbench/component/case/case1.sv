dd6653ed7691f45187098360e450001d53450dc5
william_william <472853046@qq.com>
2022-02-17 18:14:43 +0800

    update case1 and case2

--- testbench/component/case/case1.sv
+++ testbench/component/case/case1.sv
@@ -1,46 +1,114 @@
-// class case1_sequence extends uvm_sequence #(transaction_dut);
-//     transaction_dut m_trans;
+`ifndef _CASE1_SV
+`define _CASE1_SV
 
-//     function  new(string name= "case1_sequence");
-//         super.new(name);
-//     endfunction 
+// -- virtual sequence for data channel and formatter
+class case1_vseq extends uvm_sequence;
+	sequence_channel chnl_seq;
+	sequence_formatter fmt_seq;
+
+	`uvm_object_utils(case1_vseq)
+    `uvm_declare_p_sequencer(virtual_sqr)
+
+	function new(string name = "case1_vseq");
+		super.new(name);
+	endfunction
+	virtual task body();
+		if(starting_phase != null)
+			starting_phase.raise_objection(this);
+        #100;
+        `uvm_info("case1_vseq", "sequence will create", UVM_HIGH);
+
+		// set the sequence send to formatter, fmt_seq doesn't consume time
+		`uvm_do_on_with(fmt_seq, p_sequencer.fmt_sqr, {fifo==LONG_FIFO; bandwidth == HIGH_WIDTH; } );
+        
+		fork
+			begin
+		    	`uvm_do_on_with(chnl_seq, p_sequencer.chnl_sqrs[0], {ntrans == 100; ch_id==0; data_nidles==0; pkt_nidles==1; data_size==8; });
+                #500;
+            end
+            begin
+				`uvm_do_on_with(chnl_seq, p_sequencer.chnl_sqrs[1], {ntrans == 100; ch_id==1; data_nidles==1; pkt_nidles==4; data_size==16; });
+                #500;
+            end
+            begin
+				`uvm_do_on_with(chnl_seq, p_sequencer.chnl_sqrs[2], {ntrans == 100; ch_id==2; data_nidles==2; pkt_nidles==8; data_size==32; });
+			    #500;
+            end
+		join
+        
+		#10us;   // wait untill all data have been transfered through MCDF        
+		if(starting_phase != null)
+			starting_phase.drop_objection(this);
+	endtask
+
+endclass 
  
-//     virtual task body();
-//         if(starting_phase != null) 
-//             starting_phase.raise_objection(this);
-//         repeat (10) begin
-//             `uvm_do_with(m_trans, { m_trans.pload.size() == 10;})
-//         end
-//         #100;
-//         if(starting_phase != null) 
-//             starting_phase.drop_objection(this);
-//     endtask
- 
-//    `uvm_object_utils(case1_sequence)
-// endclass
+// -- virtual sequence for register
+class case1_cfg_vseq extends uvm_sequence;
+	sequence_bus reg_seq;
 
-// class case1 extends base_test;
-   
-//     `uvm_component_utils(case1)
-//     function new(string name = "case1", uvm_component parent = null);
-//         super.new(name,parent);
-//     endfunction 
-    
-//     virtual function void build_phase(uvm_phase phase);
-//         super.build_phase(phase);      
-//     endfunction
+	`uvm_object_utils(case1_cfg_vseq)
+    `uvm_declare_p_sequencer(virtual_sqr)
 
-//     virtual task main_phase(uvm_phase phase);
-//         /* 2. not use default_sequence */
-//         case1_sequence seq;
-//         seq = case1_sequence::type_id::create("seq");
-//         // set starting_phase for uvm_sequence.body() task
-//         seq.starting_phase = phase;
-//         seq.start(env.in_agt.sqr);
-//     endtask 
+	function new(string name = "case1_cfg_vseq");
+		super.new(name);
+	endfunction
+	virtual task body();
+        uvm_status_e status;
+        uvm_reg_data_t wr_val, rd_val;
+		if(starting_phase != null)
+			starting_phase.raise_objection(this);
+
+        // set slv0 ctrl reg with len=8, prio=0, en=1, and then readout to compare
+        wr_val = (1<<3) + (0<<1) + 1;
+		p_sequencer.p_rm.chnl0_ctrl_reg.write(status, wr_val);
+		p_sequencer.p_rm.chnl0_ctrl_reg.read(status, rd_val);
+        if( wr_val != rd_val)
+            `uvm_error("reg_compare", $sformatf("ERROR! %s write value 0x%8x != read value 0x%8x", "SLV0_CTRL_REG", wr_val, rd_val))
+
+        // set slv1 ctrl reg with len=16, prio=1, en=1, and then readout to compare
+        wr_val = (2<<3) + (1<<1) + 1;
+		p_sequencer.p_rm.chnl1_ctrl_reg.write(status, wr_val);
+		p_sequencer.p_rm.chnl1_ctrl_reg.read(status, rd_val);
+        if( wr_val != rd_val)
+            `uvm_error("reg_compare", $sformatf("ERROR! %s write value 0x%8x != read value 0x%8x", "SLV1_CTRL_REG", wr_val, rd_val))
+        
+        // set slv2 ctrl reg with len=32, prio=2, en=1, and then readout to compare
+        wr_val = (3<<3) + (3<<1) + 1;
+		p_sequencer.p_rm.chnl2_ctrl_reg.write(status, wr_val);
+		p_sequencer.p_rm.chnl2_ctrl_reg.read(status, rd_val);
+        if( wr_val != rd_val)
+            `uvm_error("reg_compare", $sformatf("ERROR! %s write value 0x%8x != read value 0x%8x", "SLV2_CTRL_REG", wr_val, rd_val))
+
+		// send IDLE command
+		`uvm_do_on_with(reg_seq, p_sequencer.reg_sqr, {cmd==`IDLE; } )
+		
+        #500;
+		if(starting_phase != null)
+			starting_phase.drop_objection(this);
+	endtask
+
+endclass 
 
 
-// endclass
+class case1 extends base_test;
+	`uvm_component_utils(case1)
+	
+	function new(string name = "case1", uvm_component parent = null);
+		super.new(name, parent);
+	endfunction
+	
+	virtual function void build_phase(uvm_phase phase);
+		super.build_phase(phase);
+		
+        uvm_config_db #(uvm_object_wrapper)::set(this, "v_sqr.configure_phase", "default_sequence", case1_cfg_vseq::type_id::get());
+		uvm_config_db #(uvm_object_wrapper)::set(this, "v_sqr.main_phase", "default_sequence", case1_vseq::type_id::get());
 
+	endfunction
+	
+endclass
+
+	
+`endif
 
  
--- testbench/component/case/case2.sv
+++ testbench/component/case/case2.sv
@@ -1,214 +1,123 @@
-// `ifndef _CASE2_SV
-// `define _CASE2_SV
+`ifndef _CASE2_SV
+`define _CASE2_SV
 
-// class case2_sequence extends uvm_sequence #(transaction_dut);
-// 	transaction_dut m_trans;
+// -- virtual sequence for data channel and formatter
+// full rand test
+class case2_vseq extends uvm_sequence;
+	sequence_channel chnl_seq;
+	sequence_formatter fmt_seq;
 
-// 	`uvm_object_utils(case2_sequence)
-// 	function new(string name = "case2_sequence");
-// 		super.new(name);
-// 	endfunction
+	`uvm_object_utils(case2_vseq)
+    `uvm_declare_p_sequencer(virtual_sqr)
 
-// 	virtual task body();
-//         repeat(5) begin
-// 		    // `uvm_info("case2_sequence", "send one transaction to sequencer", UVM_MEDIUM)
-// 			`uvm_do(m_trans)
-// 		end
-// 		#1000;
-// 	endtask
-// endclass
+	function new(string name = "case2_vseq");
+		super.new(name);
+	endfunction
+	virtual task body();
+		if(starting_phase != null)
+			starting_phase.raise_objection(this);
+        #100;
+        `uvm_info("case2_vseq", "sequence will create", UVM_HIGH);
 
-// // -- virtual sequence for case2
-// class case2_vseq extends uvm_sequence;
-// 	`uvm_object_utils(case2_vseq)
-//     `uvm_declare_p_sequencer(virtual_sequencer)
-
-// 	function new(string name = "case2_vseq");
-// 		super.new(name);
-// 	endfunction
-// 	virtual task body();
-//         case2_sequence dut_seq;
-//         uvm_status_e status;
-//         uvm_reg_data_t value;
-
-// 		if(starting_phase != null)
-// 			starting_phase.raise_objection(this);
+		// set the sequence send to formatter, fmt_seq doesn't consume time
+		`uvm_do_on_with(fmt_seq, p_sequencer.fmt_sqr, {fifo inside {SHORT_FIFO, ULTRA_FIFO}; bandwidth inside {LOW_WIDTH, ULTRA_WIDTH}; } );
         
-//      //    #1000;
-//         p_sequencer.p_rm.counter.mirror(status, UVM_NO_CHECK, UVM_FRONTDOOR);
+		fork
+			begin
+		    	`uvm_do_on_with(chnl_seq, p_sequencer.chnl_sqrs[0], {ntrans inside {[400:600]}; ch_id==0; data_nidles inside {[0:3]}; pkt_nidles inside {1,2,4,8}; data_size inside {8,16,32}; });
+                #500;
+            end
+            begin
+		    	`uvm_do_on_with(chnl_seq, p_sequencer.chnl_sqrs[1], {ntrans inside {[400:600]}; ch_id==1; data_nidles inside {[0:3]}; pkt_nidles inside {1,2,4,8}; data_size inside {8,16,32}; });
+                #500;
+            end
+            begin
+		    	`uvm_do_on_with(chnl_seq, p_sequencer.chnl_sqrs[2], {ntrans inside {[400:600]}; ch_id==2; data_nidles inside {[0:3]}; pkt_nidles inside {1,2,4,8}; data_size inside {8,16,32}; });
+			    #500;
+            end
+		join
         
-//         p_sequencer.p_rm.counter.peek(status, value);
-//         `uvm_info("case2_vseq", $sformatf("counter's value is %0h", value), UVM_LOW);
+		#10us;   // wait untill all data have been transfered through MCDF        
+		if(starting_phase != null)
+			starting_phase.drop_objection(this);
+	endtask
 
-// 		#100000;
-// 		`uvm_do_on(dut_seq, p_sequencer.p_dut_sqr)
-		
-//         if(starting_phase != null)
-// 			starting_phase.drop_objection(this);
-// 	endtask
-	
-	
-// endclass 
+endclass 
  
-// class case2_bus_sequence extends uvm_sequence #(transaction_bus);
-// 	transaction_bus m_trans;
+// -- virtual sequence for register
+  //Follow the instructions below
+  //  -reset the register block
+  //  -set all value of WR registers via uvm_reg::set()
+  //  -update them via uvm_reg_block::update()
+  //  -compare the register value via uvm_reg::mirror() with backdoor access
+class case2_cfg_vseq extends uvm_sequence;
+	sequence_bus reg_seq;
 
-// 	function new(string name = "case2_bus_sequence");
-// 		super.new(name);
-// 	endfunction
+	`uvm_object_utils(case2_cfg_vseq)
+    `uvm_declare_p_sequencer(virtual_sqr)
 
-// 	virtual task body();
-// 		if(starting_phase != null)
-// 			starting_phase.raise_objection(this);
-        
-// 		`uvm_do_with(m_trans, { m_trans.addr == 16'h9;
-//                                 m_trans.bus_op == BUS_RD;});
-//         `uvm_info("case2_bus_sequencee", $sformatf("invert's initial value is %0h", m_trans.rd_data), UVM_MEDIUM)
-// 		`uvm_do_with(m_trans, { m_trans.addr == 16'h9;
-//                                 m_trans.bus_op == BUS_WR;
-//                                 m_trans.wr_data == 16'h1;});
-// 		`uvm_do_with(m_trans, { m_trans.addr == 16'h9;
-//                                 m_trans.bus_op == BUS_RD;});
-//         `uvm_info("case2_bus_sequencee", $sformatf("after set, invert's value is %0h", m_trans.rd_data), UVM_MEDIUM)
-// 		`uvm_do_with(m_trans, { m_trans.addr == 16'h9;
-//                                 m_trans.bus_op == BUS_WR;
-//                                 m_trans.wr_data == 16'h0;});
-// 		`uvm_do_with(m_trans, { m_trans.addr == 16'h9;
-//                                 m_trans.bus_op == BUS_RD;});
-//         `uvm_info("case2_bus_sequencee", $sformatf("after set, invert's value is %0h", m_trans.rd_data), UVM_MEDIUM)
-// 		`uvm_do_with(m_trans, { m_trans.addr == 16'h9;
-//                                 m_trans.bus_op == BUS_WR;
-//                                 m_trans.wr_data == 16'h1;});
-// 		`uvm_do_with(m_trans, { m_trans.addr == 16'h9;
-//                                 m_trans.bus_op == BUS_RD;});
-//         `uvm_info("case2_bus_sequencee", $sformatf("after set, invert's value is %0h", m_trans.rd_data), UVM_MEDIUM)
+	function new(string name = "case2_cfg_vseq");
+		super.new(name);
+	endfunction
+	virtual task body();
+        uvm_status_e status;
+        uvm_reg_data_t ch0_wr_val, ch1_wr_val, ch2_wr_val;
+		if(starting_phase != null)
+			starting_phase.raise_objection(this);
 
-// 		#1000;
-// 		if(starting_phase != null)
-// 			starting_phase.drop_objection(this);
-// 	endtask
-	
-// 	`uvm_object_utils(case2_bus_sequence)
-	
-// endclass 
+        //reset the register block
+        p_sequencer.p_rm.reset();
 
-// // -- virtual sequence for case2 register model
-// class case2_cfg_vseq extends uvm_sequence;
-// 	`uvm_object_utils(case2_cfg_vseq)
-//     `uvm_declare_p_sequencer(virtual_sequencer)
+        //slv0 with len={4,8,16,32},  prio={[0:3]}, en={[0:1]}
+        ch0_wr_val = ($urandom_range(0,3)<<3)+($urandom_range(0,3)<<1)+$urandom_range(0,1);
+        ch1_wr_val = ($urandom_range(0,3)<<3)+($urandom_range(0,3)<<1)+$urandom_range(0,1);
+        ch2_wr_val = ($urandom_range(0,3)<<3)+($urandom_range(0,3)<<1)+$urandom_range(0,1);
 
-// 	function new(string name = "case2_cfg_vseq");
-// 		super.new(name);
-// 	endfunction
-// 	virtual task body();
-//         uvm_status_e status;
-//         uvm_reg_data_t value;
-//         uvm_reg_mem_hdl_paths_seq ckseq;
-// //      bit[31:0] counter;
+        //set all value of WR registers via uvm_reg::set() 
+        p_sequencer.p_rm.chnl0_ctrl_reg.set(ch0_wr_val);
+        p_sequencer.p_rm.chnl1_ctrl_reg.set(ch1_wr_val);
+        p_sequencer.p_rm.chnl2_ctrl_reg.set(ch2_wr_val);
 
-// 		if(starting_phase != null)
-// 			starting_phase.raise_objection(this);
-        
-//         #1000;
-//         ckseq = new("ckseq");
-//         ckseq.model = p_sequencer.p_rm;
-//         ckseq.start(null);
+        //update them via uvm_reg_block::update()
+        p_sequencer.p_rm.update(status);
 
-//         p_sequencer.p_rm.invert.set(16'h1);
-//         value = p_sequencer.p_rm.invert.get();
-//         `uvm_info("case2_cfg_vseq", $sformatf("invert's desired value is %0h", value), UVM_LOW)
-        
-//         value = p_sequencer.p_rm.invert.get_mirrored_value();
-//         `uvm_info("case2_cfg_vseq", $sformatf("invert's mirrored value is %0h", value), UVM_LOW)
-        
-//         p_sequencer.p_rm.invert.update(status, UVM_FRONTDOOR);
-//         value = p_sequencer.p_rm.invert.get();
-//         `uvm_info("case2_cfg_vseq", $sformatf("invert's desired value is %0h", value), UVM_LOW)
-//         value = p_sequencer.p_rm.invert.get_mirrored_value();
-//         `uvm_info("case2_cfg_vseq", $sformatf("invert's mirrored value is %0h", value), UVM_LOW)
-//         p_sequencer.p_rm.invert.peek(status, value);
-//         `uvm_info("case2_cfg_vseq", $sformatf("invert's actual value is %0h", value), UVM_LOW)
+        //wait until the registers in DUT have been updated
+        #100ns;
 
-// /*        p_sequencer.p_rm.counter.read(status, value, UVM_FRONTDOOR);
-//         `uvm_info("case2_cfg_vseq", $sformatf("counter's initial value(FRONTDOOR) is %0h", value), UVM_LOW)
-        
-//         p_sequencer.p_rm.counter.poke(status, 32'h1_fffd);
-//         p_sequencer.p_rm.counter.read(status, value, UVM_FRONTDOOR);
-//         `uvm_info("case2_cfg_vseq", $sformatf("after poke, counter's value(FRONTDOOR) is %0h", value), UVM_LOW)
-
-//         p_sequencer.p_rm.counter.peek(status, value);
-//         `uvm_info("case2_cfg_vseq", $sformatf("after poke, counter's value(BACKDOOR) is %0h", value), UVM_LOW)
-// */
-// /*        p_sequencer.p_rm.counter_low.poke(status, 16'hfffd);
-//         p_sequencer.p_rm.counter_low.read(status, value, UVM_FRONTDOOR);
-//         counter[15:0] = value[15:0];
-//         p_sequencer.p_rm.counter_high.read(status, value, UVM_FRONTDOOR);
-//         counter[31:16] = value[15:0];
-//         `uvm_info("case2_cfg_vseq", $sformatf("after poke, counter's value(FRONTDOOR) is %0h", counter), UVM_LOW)
-
-//         p_sequencer.p_rm.counter_low.peek(status, value);
-//         counter[15:0] = value[15:0];
-//         p_sequencer.p_rm.counter_high.peek(status, value);
-//         counter[31:16] = value[15:0];
-//         `uvm_info("case2_cfg_vseq", $sformatf("after poke, counter's value(BACKDOOR) is %0h", counter), UVM_LOW)
-// */
-// //		`uvm_do_on(bus_seq, p_sequencer.p_bus_sqr)
+        //compare all of write value and read value
+        p_sequencer.p_rm.chnl0_ctrl_reg.mirror(status, UVM_CHECK, UVM_BACKDOOR);
+        p_sequencer.p_rm.chnl1_ctrl_reg.mirror(status, UVM_CHECK, UVM_BACKDOOR);
+        p_sequencer.p_rm.chnl2_ctrl_reg.mirror(status, UVM_CHECK, UVM_BACKDOOR);
 		
-//         #5000;
-// 		if(starting_phase != null)
-// 			starting_phase.drop_objection(this);
-// 	endtask
-
-// endclass 
-
-
-// class case2 extends base_test;
-//     virtual interface_backdoor vif;
-
-//     `uvm_component_utils(case2)
-// 	function new(string name = "case2", uvm_component parent = null);
-// 		super.new(name, parent);
-// 	endfunction
-	
-// 	virtual function void build_phase(uvm_phase phase);
-// 		super.build_phase(phase);
+        // send IDLE command
+		`uvm_do_on_with(reg_seq, p_sequencer.reg_sqr, {cmd==`IDLE; } )
 		
-//         /* 1. use default_sequence */
-// 		uvm_config_db #(uvm_object_wrapper)::set(this, "v_sqr.configure_phase", "default_sequence", case2_cfg_vseq::type_id::get());
-// 		uvm_config_db #(uvm_object_wrapper)::set(this, "v_sqr.main_phase", "default_sequence", case2_vseq::type_id::get());
-// 	 	uvm_config_db #(virtual interface_backdoor)::get(this, "", "vif", vif);
+        #500;
+		if(starting_phase != null)
+			starting_phase.drop_objection(this);
+	endtask
+
+endclass 
+
+
+class case2 extends base_test;
+	`uvm_component_utils(case2)
 	
-//     endfunction
+	function new(string name = "case2", uvm_component parent = null);
+		super.new(name, parent);
+	endfunction
 	
-//     virtual task pre_main_phase(uvm_phase phase);
-//         phase.raise_objection(this);
-//         // @(vif.rst_n);
-//         vif.poke_counter(32'hFFFD);
-//         phase.drop_objection(this);
-//     endtask
-    
-//     virtual task post_main_phase(uvm_phase phase);
-//         uvm_status_e status;
-//         bit[31:0] counter;
-        
-//         phase.raise_objection(this);
-        
-// /*        vif.poke_counter(32'hFD);
-//         #200;
-//         // rm.counter.write(status, 16'h55, UVM_FRONTDOOR);
+	virtual function void build_phase(uvm_phase phase);
+		super.build_phase(phase);
+		
+        uvm_config_db #(uvm_object_wrapper)::set(this, "v_sqr.configure_phase", "default_sequence", case2_cfg_vseq::type_id::get());
+		uvm_config_db #(uvm_object_wrapper)::set(this, "v_sqr.main_phase", "default_sequence", case2_vseq::type_id::get());
 
-//         counter = rm.counter.get_mirrored_value();
-//         `uvm_info(get_type_name(), $sformatf("counter's mirrored value is %0h ", counter), UVM_LOW);
-// */  
-//         rm.counter.mirror(status, UVM_CHECK, UVM_FRONTDOOR);
-// /*
-//         counter = rm.counter.get_mirrored_value();
-//         `uvm_info(get_type_name(), $sformatf("after mirror, counter's mirrored value is %0h ", counter), UVM_LOW);
-//   */      
-//         phase.drop_objection(this);
-//     endtask
-// endclass
+	endfunction
+	
+endclass
 
-// `endif
+	
+`endif
 
- 
+
